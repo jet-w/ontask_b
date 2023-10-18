17bfa0f70fb622b23adb3e4b18e819136393d157
Haiyue Wang <wanhy149@mymail.unisa.edu.au>
2023-10-18 11:49:41 +1030

    Add all canvas_api code

---
+++ canvas_api/etc/conf.py
@@ -0,0 +1,11 @@
+#--------------------------------------------------------------------------------
+#> wanhy149: RjKO0vVkPQZZOtTlKqjsZEJO373Pq10lZDXQc2afXa1Ci4kj2OEqcleevAectWLB
+#--------------------------------------------------------------------------------
+
+api_base = 'https://canvas-dev.c3l.ai'
+wanhy149_token = 'RjKO0vVkPQZZOtTlKqjsZEJO373Pq10lZDXQc2afXa1Ci4kj2OEqcleevAectWLB'
+
+
+
+#api_base = 'https://canvas.instructure.com'
+#wanhy149_token = '7~xCE3bTo9k9sUpDwL7vQJJ9vdy6xPNPGuh86qcimrEirmwHm7UbwYmmcUOtLaIOYq'
